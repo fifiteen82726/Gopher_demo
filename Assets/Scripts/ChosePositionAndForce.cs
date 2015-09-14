4d915e12a19e29deaf25c2cb915dc00eb3f6a53e
fifiteen82726 <fifiteen82726@gmail.com>
2015-09-15 01:42:10 +0800

    Luncher will keep moving and Boom will di display a new Boom

--- Assets/Scripts/ChosePositionAndForce.cs
+++ Assets/Scripts/ChosePositionAndForce.cs
@@ -22,12 +22,16 @@ public class ChosePositionAndForce : MonoBehaviour {
 	public float BoomPosition,BoomendPosition;
 	public float Boomspeed;
 
+
+
 	bool ChosePosition = false ;
 	bool ChoseForce = false;
 
 	float PowerDegree = 0f;
 
 	public static bool displayCarrierOrNot = true;
+	public static bool CarrierReset = false;
+
 	// Use this for initialization
 	void Start () {
 		iTween.MoveTo (Luncher, iTween.Hash ("x", LuncherendPosition,"speed",Luncherspeed,"EaseType",LunchereaseType,"LoopType",LuncherloopType));
@@ -39,15 +43,15 @@ public class ChosePositionAndForce : MonoBehaviour {
 
 
 
-		if (ChosePosition == true && ChoseForce == false) 
+		if (ChosePosition == true && ChoseForce == false && ) 
 		{
-			iTween.Stop (Luncher);
+			iTween.Pause (Luncher);
 			iTween.MoveTo (Star, iTween.Hash ("y", StarendPosition, "speed", Starspeed, "EaseType", StareaseType, "LoopType", StarloopType));
 		} 
 		else if (ChosePosition == true && ChoseForce == true) 
 		{
-			iTween.Stop(Luncher);
-			iTween.Stop(Star);	
+			iTween.Pause(Luncher);
+			iTween.Pause(Star);	
 		}
 
 		//if(displayCarrierCount  == true)
@@ -63,8 +67,18 @@ public class ChosePositionAndForce : MonoBehaviour {
 	}
 
 	void OnMouseDown() {	
+		
+		//print (ChosePosition.ToString() + ChoseForce.ToString() + CarrierReset.ToString());
+		
+
+		//移動中要停止
 		if (ChosePosition == false)
 			ChosePosition = true;
+
+
+		//else if (ChosePosition == false && CarrierReset == true)
+		//if (ChosePosition == false && CarrierReset != true)
+		//停止後要讓星星停止
 		else if (ChosePosition == true && ChoseForce == false) {
 			ChoseForce = true;
 			//Compute Force power 
@@ -77,7 +91,20 @@ public class ChosePositionAndForce : MonoBehaviour {
 			GameObject newBoom = Instantiate(Boom, NewBoomPostion , Quaternion.identity) as GameObject;
 			iTween.MoveTo (newBoom, iTween.Hash ("y",BoomendPosition ,"speed",Boomspeed,"EaseType",BoomeaseType,"LoopType",BoomloopType));
 			displayCarrierOrNot = false;
+			CarrierReset = true;
+			ChosePosition  = false;
+			ChoseForce = false;
+			
+			//發射完畢Luncher開始動
+			iTween.Resume(Luncher);
+			//發射玩後DisplayCarrier需要顯示新的
+			displayCarrierOrNot = true;
+
+			print (ChosePosition.ToString() + ChoseForce.ToString() + CarrierReset.ToString());
+
 
 		}
+
+		
 	}
 }
